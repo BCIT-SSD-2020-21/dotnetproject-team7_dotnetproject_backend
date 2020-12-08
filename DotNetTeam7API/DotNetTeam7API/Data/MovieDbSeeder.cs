3a462570bf896cfb9d579c78836e324377bb17af
Tilman Queitsch <tqueitsch@my.bcit.ca>
2020-12-08 11:05:35 -0800

    #2 added static genres

--- DotNetTeam7API/DotNetTeam7API/Data/MovieDbSeeder.cs
+++ DotNetTeam7API/DotNetTeam7API/Data/MovieDbSeeder.cs
@@ -34,7 +34,25 @@ namespace DotNetTeam7API.Data
         {
             return new List<Genre>()
             {
-
+                new Genre(28, "Action"),
+                new Genre(12, "Adventure"),
+                new Genre(16, "Animation"),
+                new Genre(35, "Comedy"),
+                new Genre(80, "Crime"),
+                new Genre(99, "Documentary"),
+                new Genre(18, "Drama"),
+                new Genre(10751, "Family"),
+                new Genre(14, "Fantasy"),
+                new Genre(36, "History"),
+                new Genre(27, "Horror"),
+                new Genre(10402, "Music"),
+                new Genre(9648, "Mystery"),
+                new Genre(10749, "Romance"),
+                new Genre(878, "Science Fiction"),
+                new Genre(10770, "TV Movie"),
+                new Genre(53, "Thriller"),
+                new Genre(10752, "War"),
+                new Genre(37, "Western")
             };
         }
     }
