41d0fb75f53596e06ce0e2ee4140d9b57265e8ca
thebcitguy <wow1@my.bcit.ca>
2020-12-09 21:09:26 +0800

    Add a Controller to Handle Your Auth Requests

---
+++ DotNetTeam7API/DotNetTeam7API/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace DotNetTeam7API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private IConfiguration _config;
+        private IServiceProvider _serviceProvider;
+
+        public AuthController(SignInManager<IdentityUser> signInManager,
+        UserManager<IdentityUser> userManager,
+        IConfiguration config,
+        IServiceProvider serviceProvider)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _config = config;
+            _serviceProvider = serviceProvider;
+        }
+    }
+}
