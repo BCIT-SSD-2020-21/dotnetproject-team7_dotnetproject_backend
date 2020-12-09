787b837699a8097a5943370b5c522fb3e5125c72
Tilman Queitsch <tqueitsch@my.bcit.ca>
2020-12-09 08:00:47 -0800

    #13 Added IndexVM

---
+++ DotNetTeam7API/DotNetTeam7API/ViewModels/MovieIndexVM.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetTeam7API.ViewModels
+{
+    public class MovieIndexVM
+    {
+        public List<MovieVM> Movies { get; set; }
+        public List<SelectListItem> Genres { get; set; }
+        public int? GenreFilterApplied { get; set; }
+    }
+}
