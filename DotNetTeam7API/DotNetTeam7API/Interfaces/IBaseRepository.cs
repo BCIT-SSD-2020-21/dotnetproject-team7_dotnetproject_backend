9162bdb72c70dbf79c4fd1d5bd827d379fa0299c
Tilman Queitsch <tqueitsch@my.bcit.ca>
2020-12-09 21:23:00 -0800

    #22 created movie service interface and class

--- DotNetTeam7API/DotNetTeam7API/Interfaces/IBaseRepository.cs
+++ DotNetTeam7API/DotNetTeam7API/Interfaces/IBaseRepository.cs
@@ -8,6 +8,8 @@ namespace DotNetTeam7API.Interfaces
 {
     public interface IBaseRepository<T> where T : BaseEntity
     {
+        // tQ: every interface of this type will enforce the definition of the following functionality
+        //     does not actually run query, can run filters on top of this; limits traffic to db and saves time
         IQueryable<T> GetAll();
     }
 }
