3223c5e6f3825ff203c3247c39c6ff83bf067383
Tilman Queitsch <tqueitsch@my.bcit.ca>
2020-12-15 17:43:39 -0800

    #45 completed property list

--- DotNetTeam7API/DotNetTeam7API/ViewModels/MovieVM.cs
+++ DotNetTeam7API/DotNetTeam7API/ViewModels/MovieVM.cs
@@ -9,19 +9,16 @@ namespace DotNetTeam7API.ViewModels
     {
 
         public string Backdrop_path { get; set; }
-
+        public string First_air_date { get; set; }
         public string Name { get; set; }
-
         public string Overview { get; set; }
-
         public string Poster_path { get; set; }
-
         public double TMDB_average { get; set; }
-
         public double Korflix_average { get; set; }
 
         public MovieVM(
             string backdrop_path,
+            string first_air_date,
             string name,
             string overview,
             string poster_path,
@@ -29,6 +26,7 @@ namespace DotNetTeam7API.ViewModels
             double korflix_average)
         {
             Backdrop_path = backdrop_path;
+            First_air_date = first_air_date;
             Name = name;
             Overview = overview;
             Poster_path = poster_path;
