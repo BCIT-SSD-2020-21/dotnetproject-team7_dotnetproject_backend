8f77e21158478e53e7588ed51c5ec89639dd83ab
Tilman Queitsch <tqueitsch@my.bcit.ca>
2020-12-15 18:48:29 -0800

    #46 building helper function s

--- DotNetTeam7API/DotNetTeam7API/Services/MovieService.cs
+++ DotNetTeam7API/DotNetTeam7API/Services/MovieService.cs
@@ -2,6 +2,7 @@
 using DotNetTeam7API.Models;
 using DotNetTeam7API.ViewModels;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -11,18 +12,21 @@ namespace DotNetTeam7API.Services
     {
         private readonly IBaseRepository<Movie> _movieRepo;
         private readonly IBaseRepository<Genre> _genreRepo;
+        private readonly IQueryable<MovieUser> _movieUserRepo;
 
         public MovieService(IBaseRepository<Movie> movieRepo,
-            IBaseRepository<Genre> genreRepo)
+            IBaseRepository<Genre> genreRepo,
+            IQueryable<MovieUser> movieUserRepo)
         {
             _movieRepo = movieRepo;
             _genreRepo = genreRepo;
+            _movieUserRepo = movieUserRepo;
         }
         // this is for get all it can split to create any number of collections
         // MovieIndexVM has List<MovieVm> and List<MovieDetailVm> and List<Genre>
         // simple implementation is List<MovieVM>
         // more complex is one beefy MovieIndexVM
-        public List<BaseEntity> GetMovies(int? genreId, string searchTerm)
+        public List<MovieVM> GetMovies(int? genreId, string searchTerm)
         {
             var movies = _movieRepo.GetAll();
             //var genres = _genreRepo.GetAll(); 
@@ -61,7 +65,7 @@ namespace DotNetTeam7API.Services
             return ret;
         }
 
-        public Movie GetById(int movieId)
+        public MovieVM GetById(int movieId)
         {
             //.Include(m => m.MovieGenres)
             //        .ThenInclude(g => g.Genre)
@@ -74,6 +78,30 @@ namespace DotNetTeam7API.Services
                 .Where(m => m.Id == movieId)
                 .FirstOrDefault();
             return movie_ret;
+        }
+
+        private List<MovieVM> MapToMovieVM(List<Movie> movies)
+        {
+            return movies.Select(m => new MovieVM
+            {
+                Backdrop_path = m.Backdrop_path,
+                First_air_date = m.First_air_date,
+                Name = m.Name,
+                Overview = m.Overview,
+                Poster_path = m.Poster_path,
+                // tQ: max for TMDB is 10, ours is 5, hence dividing by 2
+                TMDB_average = m.Vote_average / 2,
+                Korflix_average = GetAvgRating(m)
+        }).ToList();
+        }
+
+        private double GetAvgRating(Movie movie)
+        {
+            var avg = _movieUserRepo
+                .Where(mu => mu.MovieId == movie.Id)
+                .Select(mu => mu.Rating)
+                .ToList().Average();
+            return Math.Round(avg, 1);
         }
     }
 }
